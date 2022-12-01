b390cc6b83c5f598cfd32f990bcfb1dced0f3a93
chenelias <elias9988110@gmail.com>
2022-12-01 23:00:10 +0800

    update tab icon and site theme

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-document.title = 'New Tab'
+document.title = 'New Tab // Poundd'
 ReactDOM.render(<App />, document.getElementById('root'))
