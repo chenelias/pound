960adfd2f81da868ff1474b93445e5cccab6f308
chenelias <elias9988110@gmail.com>
2022-12-07 00:30:02 +0800

    add file upload box

--- src/components/SettingMenu.jsx
+++ src/components/SettingMenu.jsx
@@ -226,40 +226,25 @@ const SettingMenu = ({
                     </div>
                     <div className="p-2 block text-md items-center ">
                         <p>Background Image</p>
-
-                        <div class="flex items-center justify-center w-full mt-1 mx-auto ">
-                            <label
-                                for="fileinputbox"
-                                class="flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 h-[130px] "
-                            >
-                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                    <svg
-                                        aria-hidden="true"
-                                        class="w-10 h-10 mb-3 text-gray-400"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                                        ></path>
-                                    </svg>
-                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                                        <span class="font-semibold">Click to upload</span> or drag and drop
-                                    </p>
-                                    <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG, JEPG</p>
-                                </div>
-                                <input
-                                    type="file"
-                                    id="fileinputbox"
-                                    onChange={(x) => fileInput(x)}
-                                    className="outline-none focus:ring-2 ring-blue-500hidden"
-                                />
-                            </label>
+                        <div className="flex flex-col mt-1 items-center justify-center w-full h-[120px] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                            <div className="absolute rounded-md items-center">
+                              
+                                <p className="text-center text-md">
+                                    <span className="font-medium">Click</span> to upload
+                                    <br />
+                                    or
+                                    <br />
+                                    <span className="font-medium">drop</span> and{' '}
+                                    <span className="font-medium">drop</span>
+                                </p>
+                            </div>
+                            <input
+                                type="file"
+                                id="fileinputbox"
+                                name="fileinputbox"
+                                onChange={(x) => fileInput(x)}
+                                className="cursor-pointer opacity-0 !h-[120px] w-full rounded-md "
+                            />
                         </div>
                     </div>
                     {backgroundImage && (
