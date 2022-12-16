bea29b430b58948e9034ab6c2d1e9723a2758dee
chenelias <elias9988110@gmail.com>
2022-12-16 22:58:32 +0800

    add backgroundImage

---
+++ src/image/exportImage.jsx
@@ -0,0 +1,20 @@
+export const exportImage = [
+    {
+        filename: 'pexels-aleksandar-pasaric-3629227',
+    },
+    {
+        filename: 'pexels-craig-adderley-1563356',
+    },
+    {
+        filename: 'pexels-eberhard-grossgasteiger-1428277',
+    },
+    {
+        filename: 'pexels-louis-2101187',
+    },
+    {
+        filename: 'pexels-rok-romih-3848158',
+    },
+    {
+        filename: 'pexels-steve-johnson-1269968',
+    },
+]
